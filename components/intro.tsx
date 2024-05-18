05244d118b75307e64d7684ced6efb52f6781005
Nicholas Gutierrez <118782325+nicholasg2001@users.noreply.github.com>
2024-05-18 08:31:16 -0400

    fix linkedin href

--- components/intro.tsx
+++ components/intro.tsx
@@ -98,7 +98,7 @@ export default function Intro() {
 
         <a
           className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-          href="https://linkedin.com"
+          href="https://linkedin.com/in/nicholas-gutierrez12"
           target="_blank"
         >
           <BsLinkedin />
