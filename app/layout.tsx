68b7ebb7110d1f3ec9747fcc9308a84fc95b1ddd
Nicholas Gutierrez <118782325+nicholasg2001@users.noreply.github.com>
2024-06-01 09:55:26 -0400

    add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import Footer from "@/components/footer";
 import ThemeSwitch from "@/components/theme-switch";
 import ThemeContextProvider from "@/context/theme-context";
 import { Toaster } from "react-hot-toast";
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -28,6 +29,7 @@ export default function RootLayout({
 
         <ThemeContextProvider>
           <ActiveSectionContextProvider>
+          <Analytics />
             <Header />
             {children}
             <Footer />
