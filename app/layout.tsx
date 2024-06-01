405f3c3b92401cf8f1275b2e3588011fb52f2b7c
Nicholas Gutierrez <118782325+nicholasg2001@users.noreply.github.com>
2024-06-01 09:51:17 -0400

    reformat title

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ import { Toaster } from "react-hot-toast";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Nick | Personal Portfolio",
+  title: "Nick | Portfolio",
 };
 
 export default function RootLayout({
